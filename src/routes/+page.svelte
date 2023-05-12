24304847d538028ee5f92fa4b07d5b9aa5618d8a
Garrett Nelson <gjnels@proton.me>
2023-05-12 11:15:18 -0400

    Move Ship component to components folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { ships } from '$lib/game/ships'
-  import Ship from './Ship.svelte'
+  import Ship from '$lib/components/Ship.svelte'
 </script>
 
 <h1 class="my-2 text-center text-3xl font-bold">Battleship</h1>
