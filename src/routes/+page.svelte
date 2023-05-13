b78f86726fb63299fd1b1e4c5ac5c963752e8973
Garrett Nelson <gjnels@proton.me>
2023-05-13 18:13:02 -0400

    Add page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,8 @@
 
 <h1 class="my-2 text-center text-3xl font-bold">Battleship</h1>
 
-<div class="mt-10 flex flex-wrap-reverse justify-center gap-8">
+<h2 class="my-4 text-center text-2xl font-medium">Place your ships</h2>
+<div class="mt-4 flex flex-wrap-reverse justify-center gap-8">
   <div class="w-60 space-y-4">
     {#each $ships as ship}
       <Ship {ship} />
